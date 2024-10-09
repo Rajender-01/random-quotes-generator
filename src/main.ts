53b468b949315595c62295af4021b20e0ad2dc92
Rajender kumar <kumar.rajender@rwit.io>
2024-10-09 11:54:16 +0530

    feat: Add functionality to fetch random quotes from API

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,20 @@
 import "./scss/reset.scss";
 import "./scss/quote.scss";
+
+const button = document.querySelector("#quoteBtn");
+const API_URL = "https://api.quotable.io/random";
+
+const GetRandomQuotes = async (URL: string) => {
+  try {
+    const response = await fetch(URL);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    console.log("data", data);
+  } catch (error) {
+    console.error("Failed to fetch quote:", error);
+  }
+};
+
+button?.addEventListener("click", () => GetRandomQuotes(API_URL));
