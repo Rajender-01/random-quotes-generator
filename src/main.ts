a5d54d84794024ba6f4a4c9c4fd3f99a29627c2f
Rajender kumar <kumar.rajender@rwit.io>
2024-10-09 14:55:51 +0530

    feat: Add sharing functionality for Twitter and Facebook

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import "./scss/quote.scss";
 const Button = document.querySelector("#quoteBtn");
 const Quote = document.querySelector(".quotes__quote");
 const Author = document.querySelector(".quotes__author");
+const TwitterBtn = document.querySelector(".quotes__twitterbtn");
+const FacebookBtn = document.querySelector(".facebookbtn");
 
 const API_URL = "https://api.quotable.io/random";
 
@@ -23,8 +25,29 @@ const GetRandomQuotes = async (URL: string) => {
   }
 };
 
+const tweet = () => {
+  window.open(
+    `https://twitter.com/intent/tweet?text=${Quote?.textContent}`,
+    "Tweet Window",
+    "width=800 height=600"
+  );
+};
+
+const facebook = () => {
+  window.open(
+    `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(
+      window.location.href
+    )}&quote=${encodeURIComponent(Quote?.textContent || "")}`,
+    "Share on Facebook",
+    "width=800,height=600"
+  );
+};
+
 Button?.addEventListener("click", async () => {
   Button.textContent = "Loading...";
   await GetRandomQuotes(API_URL);
   Button.textContent = "Get Quote";
 });
+
+TwitterBtn?.addEventListener("click", tweet);
+FacebookBtn?.addEventListener("click", facebook);
